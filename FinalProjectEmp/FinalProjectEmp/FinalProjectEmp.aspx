66f2816ea9c27bdea2386fa36b51fa2625c24984
Cris Yumol <zirch_o2@yahoo.com>
2016-07-17 16:55:16 -0400

    Added Final Project Employee

---
+++ FinalProjectEmp/FinalProjectEmp/FinalProjectEmp.aspx
@@ -0,0 +1,110 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FinalProjectEmp.aspx.cs" Inherits="FinalProjectEmp.FinalProjectEmp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="margin-left: 120px">
+    
+        <asp:Label ID="Label4" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Overline="False" Font-Size="X-Large" Font-Underline="True" Text="Payroll Application"></asp:Label>
+        <br />
+        <br />
+        <br />
+    
+        <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium">First Name:</asp:Label>
+    
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtEmpFname" runat="server" Font-Names="Microsoft Sans Serif" Font-Size="Medium" style="margin-left: 3px" MaxLength="35"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+        <asp:RequiredFieldValidator ID="rfFirstName" runat="server" ControlToValidate="txtEmpFname" ErrorMessage="First Name is Missing" ForeColor="Red" ToolTip="First Name must be entered" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+        &nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblErrorFName" runat="server" ForeColor="Red" Text="Label" Visible="False"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label13" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Text="Last Name:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtEmpLname" runat="server" Font-Names="Microsoft Sans Serif" Font-Size="Medium" MaxLength="50"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+        <asp:RequiredFieldValidator ID="rfLastName" runat="server" ControlToValidate="txtEmpLname" ErrorMessage="Last Name is Missing" ForeColor="Red" ToolTip="Last Name must be entered" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+        &nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblErrorLName" runat="server" ForeColor="Red" Text="Label" Visible="False"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Text="Number of hours per week:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtNumHours" runat="server" Font-Names="Microsoft Sans Serif" Font-Size="Medium" style="margin-left: 0px"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+        <asp:RequiredFieldValidator ID="rfNumHours" runat="server" ControlToValidate="txtNumHours" ErrorMessage="Number of hours is Missing" ForeColor="Red" ToolTip="Number of Hours must be entered" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblErrorHours" runat="server" ForeColor="Red" Text="Label" Visible="False"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Text="Rate per hour:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtHourlyRate" runat="server" Font-Names="Microsoft Sans Serif" Font-Size="Medium"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+        <asp:RequiredFieldValidator ID="rfRate" runat="server" ControlToValidate="txtHourlyRate" ErrorMessage="Hourly rate is Missing" ForeColor="Red" ToolTip="Hourly rate must be entered" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblErrorRate" runat="server" ForeColor="Red" Text="Label" Visible="False"></asp:Label>
+        <br />
+        <br />
+        <asp:ValidationSummary ID="ValidationSummary1" runat="server" BorderStyle="Outset" Font-Size="Large" ForeColor="Red" Width="273px" />
+        <br />
+        <br />
+        <asp:Button ID="btnCalculate" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" OnClick="btnCalculate_Click" Text="Calculate Pay" />
+    
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnClear" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" OnClick="btnClear_Click" Text="Clear" CausesValidation="False" />
+    
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Employee's name:" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Visible="False"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:TextBox ID="txtEmpNameOut" runat="server" Enabled="False" ReadOnly="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" BorderStyle="Outset" Visible="False" Width="250px"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
+        <br />
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="Number of hours per week:" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Visible="False"></asp:Label>
+        &nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtNumHoursOut" runat="server" Enabled="False" Font-Names="Microsoft Sans Serif" Font-Size="Medium" BorderStyle="Outset" ReadOnly="True" Visible="False"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label7" runat="server" Text="Rate per hour:" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Visible="False"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtHourlyRateOut" runat="server" Enabled="False" Font-Names="Microsoft Sans Serif" Font-Size="Medium" BorderStyle="Outset" ReadOnly="True" Visible="False"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label11" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Text="Regular Pay:" Visible="False"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtRegPayOut" runat="server" Enabled="False" Font-Names="Microsoft Sans Serif" Font-Size="Medium" BorderStyle="Outset" ReadOnly="True" Visible="False"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label10" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Text="Overtime hours:" Visible="False"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtOverHourOut" runat="server" Enabled="False" Font-Names="Microsoft Sans Serif" Font-Size="Medium" BorderStyle="Outset" ReadOnly="True" Visible="False"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label12" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Text="Overtime Pay:" Visible="False"></asp:Label>
+        &nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtOverPayOut" runat="server" Enabled="False" Font-Names="Microsoft Sans Serif" Font-Size="Medium" BorderStyle="Outset" ReadOnly="True" Visible="False"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label9" runat="server" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Text="Tax deduction:" Visible="False"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtTaxOut" runat="server" Enabled="False" Font-Names="Microsoft Sans Serif" Font-Size="Medium" BorderStyle="Outset" ReadOnly="True" Visible="False"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label8" runat="server" Text="Net pay:" Font-Bold="True" Font-Names="Microsoft Sans Serif" Font-Size="Medium" Visible="False"></asp:Label>
+    
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    
+        <asp:TextBox ID="txtNetPayOut" runat="server" Enabled="False" Font-Names="Microsoft Sans Serif" Font-Size="Medium" BorderStyle="Outset" ReadOnly="True" Visible="False"></asp:TextBox>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ FinalProjectEmp/FinalProjectEmp/FinalProjectEmp.aspx.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalProjectEmp
+{
+    public partial class FinalProjectEmp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                SetFocus(txtEmpFname);
+            }
+        }
+        /// <summary>
+        /// Clears the information on the fields
+        /// </summary>
+        public void Clear_Screen()
+        {
+            //clear the input fields
+            txtEmpFname.Text = string.Empty;
+            txtEmpLname.Text = string.Empty;
+            txtNumHours.Text = string.Empty;
+            txtHourlyRate.Text = string.Empty;
+
+            //clear the output fields
+            txtEmpNameOut.Text = string.Empty;
+            txtNumHoursOut.Text = string.Empty;
+            txtHourlyRateOut.Text = string.Empty;
+            txtTaxOut.Text = string.Empty;
+            txtNetPayOut.Text = string.Empty;
+            txtRegPayOut.Text = string.Empty;
+            txtOverHourOut.Text = string.Empty;
+            txtOverPayOut.Text = string.Empty;
+
+            //clear error messages
+            lblErrorFName.Text = String.Empty;
+            lblErrorLName.Text = String.Empty;
+            lblErrorHours.Text = String.Empty;
+            lblErrorRate.Text = String.Empty;
+
+            //makes the output fields disapper
+            Label5.Visible = false;
+            txtEmpNameOut.Visible = false;
+            Label6.Visible = false;
+            txtNumHoursOut.Visible = false;
+            Label7.Visible = false;
+            txtHourlyRateOut.Visible = false;
+            Label11.Visible = false;
+            txtRegPayOut.Visible = false;
+            Label10.Visible = false;
+            txtOverHourOut.Visible = false;
+            Label12.Visible = false;
+            txtOverPayOut.Visible = false;
+            Label9.Visible = false;
+            txtTaxOut.Visible = false;
+            Label8.Visible = false;
+            txtNetPayOut.Visible = false;
+
+            SetFocus(txtEmpFname);
+        }
+
+        /// <summary>
+        /// It converts the first character of the string to uppercase
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public string UpperCaseFirstChar(string text)
+        {
+            return (text[0].ToString().ToUpper() + text.Substring(1, text.Length - 1));
+        }
+
+        /// <summary>
+        /// Calculates the regular salary of an employee
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <param name="rate"></param>
+        /// <returns></returns>
+        public decimal CalculateRegPay(decimal hours, decimal rate)
+        {
+            return hours * rate;
+        }
+
+        /// <summary>
+        /// Validates the string if it can be converted to decimal
+        /// </summary>
+        /// <param name="strInput"></param>
+        /// <returns></returns>
+        public bool Validate_Decimal(string strInput)
+        {
+            decimal decInput = 0.0M;
+            try
+            {
+                decInput = Convert.ToDecimal(strInput);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Validates the inputted first and last name
+        /// </summary>
+        /// <param name="strVal"></param>
+        /// <param name="txtbox"></param>
+        /// <returns></returns>
+        public bool Validate_Name(string strVal, TextBox txtbox)
+        {
+            bool returnVal = false;
+
+            strVal = strVal.Replace("0", "");
+            strVal = strVal.Replace("1", "");
+            strVal = strVal.Replace("2", "");
+            strVal = strVal.Replace("3", "");
+            strVal = strVal.Replace("4", "");
+            strVal = strVal.Replace("5", "");
+            strVal = strVal.Replace("6", "");
+            strVal = strVal.Replace("7", "");
+            strVal = strVal.Replace("8", "");
+            strVal = strVal.Replace("9", "");
+
+            strVal = strVal.Replace("~", "");
+            strVal = strVal.Replace("!", "");
+            strVal = strVal.Replace("@", "");
+            strVal = strVal.Replace("#", "");
+            strVal = strVal.Replace("$", "");
+            strVal = strVal.Replace("%", "");
+            strVal = strVal.Replace("^", "");
+            strVal = strVal.Replace("&", "");
+            strVal = strVal.Replace("*", "");
+            strVal = strVal.Replace("(", "");
+            strVal = strVal.Replace(")", "");
+            strVal = strVal.Replace("_", "");
+            strVal = strVal.Replace("+", "");
+            strVal = strVal.Replace("{", "");
+            strVal = strVal.Replace("}", "");
+            strVal = strVal.Replace("|", "");
+            strVal = strVal.Replace(":", "");
+            strVal = strVal.Replace("\"", "");
+            strVal = strVal.Replace("<", "");
+            strVal = strVal.Replace(">", "");
+            strVal = strVal.Replace("?", "");
+            strVal = strVal.Replace("`", "");
+            strVal = strVal.Replace("-", "");
+            strVal = strVal.Replace("=", "");
+            strVal = strVal.Replace("[", "");
+            strVal = strVal.Replace("]", "");
+            strVal = strVal.Replace("\\", "");
+            strVal = strVal.Replace(";", "");
+            strVal = strVal.Replace("'", "");
+            strVal = strVal.Replace(",", "");
+            strVal = strVal.Replace(".", "");
+            strVal = strVal.Replace("/", "");
+
+            txtbox.Text = strVal;
+
+            if (strVal.Length > 0)
+            {
+                returnVal = true;
+            }
+            else
+            {
+                returnVal = false;
+            }
+            return returnVal;
+
+        }
+
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //declare, initialize variables and uses Trim() to remove leading and trailing spaces
+            string strEmpFName = txtEmpFname.Text.Trim();
+            string strEmpLName = txtEmpLname.Text.Trim();
+            string strNumHours = txtNumHours.Text.Trim();
+            string strHourlyRate = txtHourlyRate.Text.Trim();
+
+            string strTaxDeduct = string.Empty;
+            string strGrossPay = string.Empty;
+            string strNetPay = string.Empty;
+            string strRegPay = string.Empty;
+            string strOverHour = string.Empty;
+            string strOverPay = string.Empty;
+
+            decimal decNumHours = 0.0M;
+            decimal decHourlyRate = 0.0M;
+            decimal decGrossPay = 0.0M;
+            decimal decTaxDeduct = 0.0M;
+            decimal decTaxRate = .24M;
+            decimal decNetPay = 0.0M;
+
+            decimal decRegHour = 40.0M;
+            decimal decRegPay = 0M;
+            decimal decOverHour = 0M;
+            decimal decOverPay = 0M;
+            decimal decOverRate = 1.50M;
+
+            bool flagHours = false;
+            bool flagRate = false;
+            bool flagFirstName = false;
+            bool flagLastName = false;
+
+            //validate first name
+            if (!Validate_Name(strEmpFName, txtEmpFname))
+            {
+                lblErrorFName.Text = "Invalid First name";
+                lblErrorFName.Visible = true;
+                flagFirstName = true; //this is a red flag meaning the input is incorrect
+            }
+            else
+            {
+                lblErrorFName.Text = String.Empty;
+                lblErrorFName.Visible = false;
+                flagFirstName = false; //this means that the input is correct
+
+                //convert first name to lowercase
+                strEmpFName = txtEmpFname.Text.ToLower();
+
+                //convert the first character of first name to uppercase
+                strEmpFName = UpperCaseFirstChar(strEmpFName);
+            }
+
+            //validate last name
+            if (!Validate_Name(strEmpLName, txtEmpLname))
+            {
+                lblErrorLName.Text = "Invalid Last name";
+                lblErrorLName.Visible = true;
+                flagLastName = true; //this is a red flag meaning the input is incorrect
+            }
+            else
+            {
+                lblErrorLName.Text = String.Empty;
+                lblErrorLName.Visible = false;
+                flagLastName = false;  //this means that the input is correct
+
+                //convert last name to lowercase
+                strEmpLName = txtEmpLname.Text.ToLower();
+
+                //convert the first character of the last name to uppercase
+                strEmpLName = UpperCaseFirstChar(strEmpLName);
+            }
+
+            //validates if Number of Hours is decimal
+
+            if (!Validate_Decimal(strNumHours))
+            {
+                lblErrorHours.Text = "Invalid format. Please enter a number.";
+                lblErrorHours.Visible = true;
+                flagHours = true;  //this is a red flag meaning the input is incorrect
+            }
+            else
+            {
+                lblErrorHours.Text = String.Empty;
+                lblErrorHours.Visible = false;
+                flagHours = false;  //this means that the input is correct
+
+                decNumHours = Convert.ToDecimal(strNumHours);
+
+                if (decNumHours < 0 || decNumHours > 80)
+                {
+                    lblErrorHours.Text = "Number of hours must be 0 - 80.";
+                    lblErrorHours.Visible = true;
+                    flagHours = true; //this is a red flag meaning the input is incorrect
+                }
+                else
+                {
+                    lblErrorHours.Text = String.Empty;
+                    lblErrorHours.Visible = false;
+                    flagHours = false;  //this means that the input is correct
+                }
+            }
+
+            //validates if Rate per hour is decimal
+            if (!Validate_Decimal(strHourlyRate))
+            {
+                lblErrorRate.Text = "Invalid format. Please enter a number.";
+                lblErrorRate.Visible = true;
+                flagRate = true; //this is a red flag meaning the input is incorrect
+            }
+            else
+            {
+                lblErrorRate.Text = String.Empty;
+                lblErrorRate.Visible = false;
+                flagRate = false; //this means that the input is correct
+
+                decHourlyRate = Convert.ToDecimal(strHourlyRate);
+
+                if (decHourlyRate < 1)
+                {
+                    lblErrorRate.Text = "Hourly rate must be greater than 0.";
+                    lblErrorRate.Visible = true;
+                    flagRate = true; //this is a red flag meaning the input is incorrect
+                }
+                else
+                {
+                    lblErrorRate.Text = String.Empty;
+                    lblErrorRate.Visible = false;
+                    flagRate = false; //this means that the input is correct
+                }
+            }
+            //
+            if (!flagHours && !flagRate && !flagFirstName && !flagLastName)
+            {
+                //check if the number of hours worked is more than 40 hours
+                if (decNumHours > decRegHour)
+                {
+                    decOverHour = decNumHours - decRegHour;
+                    decRegPay = CalculateRegPay(decRegHour, decHourlyRate);
+                    decOverPay = decOverHour * (decHourlyRate * decOverRate);
+                    decGrossPay = decRegPay + decOverPay;
+                }
+                else
+                {
+                    //calculate regular and gross pay
+                    decRegPay = CalculateRegPay(decNumHours, decHourlyRate);
+                    decGrossPay = decRegPay;
+                }
+
+
+                //calculate tax
+                decTaxDeduct = decGrossPay * decTaxRate;
+
+                //calculate net pay
+                decNetPay = decGrossPay - decTaxDeduct;
+
+                //convert decimal to string
+                strTaxDeduct = decTaxDeduct.ToString("c");
+                strGrossPay = decGrossPay.ToString("c");
+                strNetPay = decNetPay.ToString("c");
+                strHourlyRate = decHourlyRate.ToString("c");
+                strRegPay = decRegPay.ToString("c");
+                strOverHour = decOverHour.ToString();
+                strOverPay = decOverPay.ToString("c");
+
+                //display to textbox
+                txtEmpNameOut.Text = strEmpFName + " " + strEmpLName;
+                txtNumHoursOut.Text = strNumHours;
+                txtHourlyRateOut.Text = strHourlyRate;
+                txtTaxOut.Text = strTaxDeduct;
+                txtNetPayOut.Text = strNetPay;
+                txtRegPayOut.Text = strRegPay;
+                txtOverHourOut.Text = strOverHour;
+                txtOverPayOut.Text = strOverPay;
+
+                //show all the output labels and fields
+                Label5.Visible = true;
+                txtEmpNameOut.Visible = true;
+                Label6.Visible = true;
+                txtNumHoursOut.Visible = true;
+                Label7.Visible = true;
+                txtHourlyRateOut.Visible = true;
+                Label11.Visible = true;
+                txtRegPayOut.Visible = true;
+                Label10.Visible = true;
+                txtOverHourOut.Visible = true;
+                Label12.Visible = true;
+                txtOverPayOut.Visible = true;
+                Label9.Visible = true;
+                txtTaxOut.Visible = true;
+                Label8.Visible = true;
+                txtNetPayOut.Visible = true;
+            }
+            else
+            {
+                //clear the output fields
+                txtEmpNameOut.Text = string.Empty;
+                txtNumHoursOut.Text = string.Empty;
+                txtHourlyRateOut.Text = string.Empty;
+                txtTaxOut.Text = string.Empty;
+                txtNetPayOut.Text = string.Empty;
+                txtRegPayOut.Text = string.Empty;
+                txtOverHourOut.Text = string.Empty;
+                txtOverPayOut.Text = string.Empty;
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear_Screen();
+        }
+    }
+}
