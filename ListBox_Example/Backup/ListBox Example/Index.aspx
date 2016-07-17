ff5eaa65f4eda9cafe158fb3a0bd47017b67c01a
Cris Yumol <zirch_o2@yahoo.com>
2016-07-17 15:35:52 -0400

    Added ListBox Example

---
+++ ListBox_Example/Backup/ListBox Example/Index.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="ListBox_Example.Index" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+   
+        <br />
+        <asp:Label ID="Label4" runat="server" Font-Bold="True" Font-Size="Large" 
+            Text="Add to Team"></asp:Label>
+&nbsp;
+        <asp:DropDownList ID="dlTerm" runat="server" Font-Bold="True" Font-Size="Large">
+            <asp:ListItem Value="-1">Select Team</asp:ListItem>
+            <asp:ListItem Value="1">Green</asp:ListItem>
+            <asp:ListItem Value="2">Red</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="Large" 
+            Text="Main List"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label2" runat="server" Font-Bold="True" Font-Size="Large" 
+            Text="Green Team"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label3" runat="server" Font-Bold="True" Font-Size="Large" 
+            Text="Red Team"></asp:Label>
+        <br />
+   
+    <asp:ListBox ID="lstMembers" runat="server" Height="250px" Width="219px">
+        <asp:ListItem>Kevin Bacon</asp:ListItem>
+        <asp:ListItem>Linda Cole</asp:ListItem>
+        <asp:ListItem>Brenda Lee</asp:ListItem>
+        <asp:ListItem>Susan Fell</asp:ListItem>
+        <asp:ListItem>Maria Jose</asp:ListItem>
+        <asp:ListItem>Barney Rubble</asp:ListItem>
+        <asp:ListItem>Wilma Flintstone</asp:ListItem>
+        <asp:ListItem>Homer Simipson</asp:ListItem>
+        <asp:ListItem>Bruce Wayne</asp:ListItem>
+        <asp:ListItem>Troy edgar</asp:ListItem>
+        <asp:ListItem>Sally Smith</asp:ListItem>
+        <asp:ListItem>Kathy Brown</asp:ListItem>
+        <asp:ListItem>Howard Duck</asp:ListItem>
+        <asp:ListItem></asp:ListItem>
+        </asp:ListBox> 
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox1" runat="server" Height="250px" Width="205px">
+            <asp:ListItem>Peter Brown</asp:ListItem>
+        </asp:ListBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox2" runat="server" Height="250px" Width="205px">
+            <asp:ListItem>Lisa Stanko</asp:ListItem>
+        </asp:ListBox>
+        <br />
+        <br />
+        <asp:Button ID="Team" runat="server" Font-Bold="True" Font-Size="Large" 
+            Text="Select Term" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnClearSelection" runat="server" Font-Bold="True" 
+            Font-Size="Large" Text="Clear Selection" />
+&nbsp;&nbsp;
+        <asp:Button ID="btnResetLists" runat="server" Font-Bold="True" 
+            Font-Size="Large" Text="Reset Lists" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ListBox_Example/ListBox Example/Index.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ListBox_Example
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSelectTeam_Click(object sender, EventArgs e)
+        {
+            string strSelectedTeam = dlTeam.SelectedValue;
+            int numMemberGreen = lstGreen.Items.Count;
+            int numMemberRed = lstRed.Items.Count;
+            int totalmember = 6;
+
+            if (strSelectedTeam == "1")
+            {
+                lblErr.Text = String.Empty;
+                lblErr.Visible = false;
+                lblErrRed.Text = String.Empty;
+                lblErrRed.Visible = false;
+
+                if (numMemberGreen < totalmember)
+                {
+                    SelectTeamMembers(lstMembers, lstGreen);
+                    lblErrGreen.Text = String.Empty;
+                    lblErrGreen.Visible = false;
+
+                }
+
+                else
+                {
+                    lblErrGreen.Text = Label2.Text + " has already reached the maximum(6) number per team.";
+                    lblErrGreen.Visible = true;
+                }
+
+
+            }
+            else if (strSelectedTeam == "2")
+            {
+                lblErr.Text = String.Empty;
+                lblErr.Visible = false;
+                lblErrGreen.Text = String.Empty;
+                lblErrGreen.Visible = false;
+
+                if (numMemberRed < totalmember)
+                {
+                    SelectTeamMembers(lstMembers, lstRed);
+                    lblErrRed.Text = String.Empty;
+                    lblErrRed.Visible = false;
+                }
+                else
+                {
+                    lblErrRed.Text = Label3.Text + " has already reached the maximum(6) number per team.";
+                    lblErrRed.Visible = true;
+                }
+            }
+            else
+            {
+                lblErr.Text = "Please select a team";
+                lblErr.Visible = true;
+                lblErrGreen.Text = String.Empty;
+                lblErrGreen.Visible = false;
+                lblErrRed.Text = String.Empty;
+                lblErrRed.Visible = false;
+            }
+        }
+
+        public void SelectTeamMembers(ListBox mainlist, ListBox teamlist)
+        {
+            string selectedMember = string.Empty;
+
+            for (int i = mainlist.Items.Count - 1; i >= 0; i--)
+            {
+                if (mainlist.Items[i].Selected)
+                {
+                    selectedMember = mainlist.Items[i].Text;
+                    teamlist.Items.Add(new ListItem(selectedMember));
+                    mainlist.Items.Remove(selectedMember);
+                    lblErrMember.Text = "";
+                    lblErrMember.Visible = false;
+                }
+                else
+                {
+                    lblErrMember.Text = "Please select a team member.";
+                    lblErrMember.Visible = true;
+                }
+            }
+        }
+
+        protected void btnResetLists_Click(object sender, EventArgs e)
+        {
+            ResetTeamMember(lstMembers, lstGreen);
+            ResetTeamMember(lstMembers, lstRed);
+        }
+
+        public void ResetTeamMember(ListBox mainlist, ListBox teamlist)
+        {
+            string selectedMember = string.Empty;
+
+            for (int i = teamlist.Items.Count - 1; i >= 1; i--)
+            {
+                selectedMember = teamlist.Items[i].Text;
+                mainlist.Items.Add(new ListItem(selectedMember));
+                teamlist.Items.Remove(selectedMember);
+            }
+        }
+
+        protected void dlTeam_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblErr.Text = String.Empty;
+            lblErrGreen.Text = String.Empty;
+            lblErrRed.Text = String.Empty;
+        }
+
+        protected void btnClearSelection_Click(object sender, EventArgs e)
+        {
+            string selectedMember = string.Empty;
+
+            for (int i = lstMembers.Items.Count - 1; i >= 0; i--)
+            {
+                if (lstMembers.Items[i].Selected)
+                {
+                    selectedMember = lstMembers.Items[i].Text;
+                    lstMembers.Items.Remove(selectedMember);
+                    lblErrMember.Text = "";
+                    lblErrMember.Visible = false;
+                }
+                else
+                {
+                    lblErrMember.Text = "Please select a member.";
+                    lblErrMember.Visible = true;
+                }
+            }
+        }
+    }
+}
